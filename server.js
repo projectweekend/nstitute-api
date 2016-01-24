252d36f22416c175e4ce9aef13c7dc8a085a61e0
Brian Hines <brian@projectweekend.net>
2016-01-24 13:31:59 -0600

    Add some middleware

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var restify = require('restify');
 
-var serverConfig = {
-    name: 'nstitute-api'
-};
 
-var server = restify.createServer();
+var server = restify.createServer({
+    name: 'nstitute-api'
+});
+
+server.use(restify.CORS());
+server.use(restify.queryParser());
 
 server.listen(3000);
