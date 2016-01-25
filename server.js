3ed06359a747d4cc250171dfdf6f96b09f0b8f1e
Brian Hines <brian@projectweekend.net>
2016-01-24 19:00:30 -0600

    Add Item route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var MongoClient = require('mongodb').MongoClient;
 var config = require('./app/config');
 var middleware = require('./app/middleware');
 var nsiderArchiveCollection = require('./app/nsider-archive/collection');
+var nsiderArchiveItem = require('./app/nsider-archive/item');
 
 
 MongoClient.connect(config.mongoURL, function(err, db) {
@@ -24,6 +25,7 @@ MongoClient.connect(config.mongoURL, function(err, db) {
 
     // add routes
     server.get('/nsider-archive', nsiderArchiveCollection.get);
+    server.get('/nsider-archive/:nsiderID', nsiderArchiveItem.get);
 
     // start
     server.listen(3000);
