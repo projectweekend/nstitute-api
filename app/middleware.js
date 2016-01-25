f37211b2151bdcf004f662476dcfe74dcc99803d
Brian Hines <brian@projectweekend.net>
2016-01-24 18:33:01 -0600

    Set max take

--- app/middleware.js
+++ app/middleware.js
@@ -4,6 +4,9 @@ var exports = module.exports = {};
 exports.parseSkipTake = function(req, res, next) {
     req.skip = parseInt(req.query.skip, 10) || 0;
     req.take = parseInt(req.query.take, 10) || 10;
+    if (req.take > 50) {
+        req.take = 50;
+    }
     next();
 };
 
